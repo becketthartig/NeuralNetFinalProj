364aec8469789c6cd86c97292667072e226c106a
Beckett Hartig <becketthartig@gmail.com>
2025-11-18 09:10:38 +0100

    temp save

--- FIRST.py
+++ FIRST.py
@@ -1,8 +1,8 @@
 import csv
 
 # ---- CONFIG ----
-input_file = "oldtools/elonmusk.csv"       # your original CSV file
-output_file = "oldtools/tweettimes.csv"  # new file to create
+input_file = "elonmusk1.csv"       # your original CSV file
+output_file = "tweettimes.csv"  # new file to create
 # ----------------
 
 with open(input_file, newline='', encoding='utf-8') as infile, \
@@ -14,9 +14,16 @@ with open(input_file, newline='', encoding='utf-8') as infile, \
     writer.writerow(["date","time","char_count","is_RT"])
     char_count = 0
     is_rt = 0
+    jan1 = False
     for row in reader:
         if len(row) >= 1 and (row[-1][-3:] == 'EST' or row[-1][-3:] == 'EDT'): 
             dt = row[-1].strip('"').split(", ")
+            if dt[0] == "Jan 1":
+                jan1 = True
+            if jan1:
+                dt[0] += " 2025"
+            else:
+                dt[0] += " 2024"
             if len(row[0]) == 19 and row[0].isnumeric():
                 text = "".join(row[1:-1])
                 dt.append(len(text))
