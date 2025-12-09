a45757c8f9219e50f711d787fbb1fb471c6f0cb0
Beckett Hartig <122044284+becketthartig@users.noreply.github.com>
2025-12-09 13:12:55 +0100

    Update and rename FIRST.py to data/FIRST.py

--- data/FIRST.py
+++ data/FIRST.py
@@ -1,10 +1,13 @@
 import csv
 
-# ---- CONFIG ----
-input_file = "elonmusk1.csv"       # your original CSV file
-output_file = "tweettimes.csv"  # new file to create
-# ----------------
+input_file = "elonmusk1.csv"     
+output_file = "tweettimes.csv" 
 
+# take orignal csv and make it more workable
+# basically very complicated logic to simplify the date/time column and
+# create the char_count and is_RT (is a retweet?) features
+# Commas and new line characters in the tweet text from elonmusk.csv
+# make this a complicated process but this script is designed to handle that
 with open(input_file, newline='', encoding='utf-8') as infile, \
      open(output_file, 'w', newline='', encoding='utf-8') as outfile:
     
@@ -18,8 +21,8 @@ with open(input_file, newline='', encoding='utf-8') as infile, \
     for row in reader:
         if len(row) >= 1 and (row[-1][-3:] == 'EST' or row[-1][-3:] == 'EDT'): 
             dt = row[-1].strip('"').split(", ")
-            if dt[0] == "Jan 1":
-                jan1 = True
+            if dt[0] == "Jan 1": # this part is designed to extrapolate the date information 
+                jan1 = True      # provided and add years through its implicit ordering
             if jan1:
                 dt[0] += " 2025"
             else:
@@ -27,7 +30,7 @@ with open(input_file, newline='', encoding='utf-8') as infile, \
             if len(row[0]) == 19 and row[0].isnumeric():
                 text = "".join(row[1:-1])
                 dt.append(len(text))
-                if text[:4] == "RT @":
+                if text[:4] == "RT @": # detect a retweet in text
                     is_rt = 1
             else:
                 dt.append(char_count + len("".join(row[:-1])))
